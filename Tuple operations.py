d6c0bc60c5d959d5c2171c06be91a0bd45648e90
Farhan Shafiq Chowdhury <yfarhana044@gmail.com>
2024-09-05 09:59:15 +0600

    first commit

---
+++ Tuple operations.py
@@ -0,0 +1,16 @@
+tup =("tuple", False, 3.2, 1)
+print(tup)
+
+tuplex = (4, 6, 2, 8, 3, 1)
+print(tuplex)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (50, 10, 60, 70, 50, 50, 70, 50, )
+print(  tuple1.count(50)  )
+
+for i in range (0, len(tuple1)):
+    print(tuple1[i])
+    
+    
